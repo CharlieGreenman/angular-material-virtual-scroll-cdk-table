8c1ae133f59f351e90c046a44f2a407c992af112
Charlie Greenman <charlie@razroo.com>
2024-10-20 17:31:40 -0400

    update pipe logic

--- src/app/pipes/virtual-scroll-height.ts
+++ src/app/pipes/virtual-scroll-height.ts
@@ -1,17 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Ticket } from '@razroo-zeta/data-models';
 
 @Pipe({
   name: 'virtualScrollHeight',
   standalone: true,
 })
 export class VirtualScrollHeightPipe implements PipeTransform {
-  transform(items: any[], rowHeight = 51): number {
-    if(!items || !items?.length) {
-      return rowHeight;
-    } else if(items.length > 10) {
-      return (11 * rowHeight);
+  transform(tickets: Ticket[], rowHeight = 51, headerRowHeight = 56): number {
+    if(!tickets || !tickets?.length) {
+      // assumption is one header row + one empty row
+      return (rowHeight + headerRowHeight);
+    } else if(tickets.length > 10) {
+      return ((11 * rowHeight) + headerRowHeight);
     } else {
-      return (items?.length * rowHeight);
+      return ((tickets?.length * rowHeight) + headerRowHeight);
     }
     
   }
